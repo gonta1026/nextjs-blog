64b29a75839920d566f32dc50c8afa1ece6f4bcb
gonta1026 <katano.original@gmail.com>
2020-11-08 21:39:46 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const Home = ({allPostsData}: Posts) => {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[こんにちわ！gontaです！]</p>
+        <p>[こんにちわ！gontaですよ！]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
